d08dcbd320017164de45a2f2b1d3f1690d142145
LcsModz <137469293+LcsModz@users.noreply.github.com>
2025-02-24 00:29:32 -0300

    Update Gui_Principal.lua

--- Gui_Principal.lua
+++ Gui_Principal.lua
@@ -48,14 +48,14 @@ local function criarGUI()
 
     -- Opções do Menu Lateral
     local opcoes = {
-        {nome = "Créditos", icone = "rbxassetid://7093195125"}, -- Substituir "rbxassetid://0" pelo ID do ícone
-        {nome = "Main", icone = "rbxassetid://7093195125"},
-        {nome = "Farm", icone = "rbxassetid://7093195125"},
-        {nome = "V4", icone = "rbxassetid://7093195125"},
-        {nome = "Raid", icone = "rbxassetid://7093195125"},
-        {nome = "Teleport", icone = "rbxassetid://7093195125"},
-        {nome = "Config", icone = "rbxassetid://7093195125"},
-	    {nome = "Server Hop", icone = "rbxassetid://7093195125"}
+        {nome = "Créditos", icone = "rbxassetid://0"}, -- Substituir "rbxassetid://0" pelo ID do ícone
+        {nome = "Main", icone = "rbxassetid://0"},
+        {nome = "Farm", icone = "rbxassetid://0"},
+        {nome = "V4", icone = "rbxassetid://0"},
+        {nome = "Raid", icone = "rbxassetid://0"},
+        {nome = "Teleport", icone = "rbxassetid://0"},
+        {nome = "Config", icone = "rbxassetid://0"},
+	    {nome = "Server Hop", icone = "rbxassetid://0"}
     }
 
     local alturaOpcao = 1 / #opcoes
@@ -98,8 +98,8 @@ local function criarGUI()
         elemento.InputBegan:Connect(function(input)
             if input.UserInputType == Enum.UserInputType.MouseButton1 then
                 dragging = true
-                startPos = elemento.Position
-                mouseOffset = Vector2.new(mouse.X - elemento.AbsolutePosition.X, mouse.Y - elemento.AbsolutePosition.Y)
+startPos = elemento.Position
+                mouseOffset = Vector2.new(mouse.X - elemento.AbsolutePosition.X, mouse.Y - elemento.AbsolutePosition.Y)
             end
         end)
 
