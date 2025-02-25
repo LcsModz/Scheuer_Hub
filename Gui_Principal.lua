89f45fdde2265f52e1b3989a70d73b068966d515
LcsModz <137469293+LcsModz@users.noreply.github.com>
2025-02-25 00:38:31 -0300

    Update Gui_Principal.lua

--- Gui_Principal.lua
+++ Gui_Principal.lua
@@ -67,7 +67,7 @@ local function criarGUI()
     local espacamento = 0.01 -- Espaçamento entre os botões (ajustado para ser menor)
     local margemLateral = 0.05 -- Margem lateral para os botões
     local margemTopo = 0.02 -- Margem superior para o primeiro botão
-    local margemInterna = 0.05 -- Margem interna entre os botões e as paredes da GUI lateral (aumentada)
+    local margemInterna = 0.02 -- Margem interna entre os botões e as paredes da GUI lateral
 
     for i, opcao in ipairs(opcoes) do
         local botao = criarElemento("TextButton", {
@@ -75,7 +75,7 @@ local function criarGUI()
             BackgroundColor3 = Color3.fromRGB(70, 70, 70),
             BorderSizePixel = 0,
             Position = UDim2.new(0, margemInterna + margemLateral, alturaOpcao * (i - 1) + espacamento * (i - 1) + margemTopo + margemInterna, 0),
-            Size = UDim2.new(1, - (margemLateral * 2) - (margemInterna * 2), alturaOpcao - espacamento - (margemInterna * 2), 0), -- Ajuste no tamanho
+            Size = UDim2.new(1, - (margemLateral * 2) - (margemInterna * 2), alturaOpcao - espacamento - (margemInterna * 2), 0),
             Font = Enum.Font.SourceSansBold,
             TextColor3 = Color3.fromRGB(255, 255, 255),
             TextSize = 14,
